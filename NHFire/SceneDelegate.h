d7773a46cf018587bf86fb36d5dddff23c663cb6
matsua <matsua12@gmail.com>
2020-07-09 17:23:12 +0900

    프로젝트파일 생성

---
+++ NHFire/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  NHFire
+//
+//  Created by NH on 2020/07/06.
+//  Copyright © 2020 NH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ NHFire/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  NHFire
+//
+//  Created by NH on 2020/07/06.
+//  Copyright © 2020 NH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
